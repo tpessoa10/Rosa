0c71cef77f4926b1bac47095a96700b63c7d0859
Thiago <tpessoa96@gmail.com>
2023-08-13 22:30:01 -0300

    Estrutura inicial

---
+++ src/app.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get, Query, Render } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  // @Get()
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
+
+  @Get("/")
+  @Render("index")
+  hello(@Query('nome') nome = 'Visitante'){
+    const context = {
+      nome,
+      qtd_letras: nome.length
+    }
+    return context
+  }
+}
